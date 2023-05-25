15f7f8517e5ebc7630d9e71f248d577b894cb83f
izumiya <izumiya.shion@no1s.biz>
2023-05-25 14:06:21 +0900

    no message

---
+++ models/FormRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+class FormRepository extends DbRepository{
+
+    public function getFormModel(){
+
+        $data = [
+            "name"       => " ",
+            "age"        => " ",
+            "prefecture" => " ",
+            "address1"   => " ",
+            "address2"   => " ",
+            "comment"    => " ",
+            "mail_address" => " "
+        ];
+
+        return $data;
+    }
+
+    public function insert($form) {
+        $sql = "
+                insert into form (name, age, prefecture, address1, address2, comment, mail_address)
+                    VALUES(:name, :age, :prefecture, :address1, :address2, :comment, :mail_address)
+                    ";
+    
+                    $stmt = $this->execute($sql, $form);
+    }
+}
---
+++ views/form/complete.php
@@ -0,0 +1,6 @@
+<?php $this->setLayoutVar('title', '完了') ?>
+
+<div>
+    <h2>完了画面</h2>
+    <P><?php echo $form["name"]; ?>さん お問い合わせありがとうございます</P>
+</div>
